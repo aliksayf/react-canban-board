7edfbcc8e7090943bce122394736a6cf7bada52b
aliksayf <52054450+aliksayf@users.noreply.github.com>
2020-04-08 11:22:35 -0700

    Creat structure files

---
+++ src/StatisTable.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function StatusTable () {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default StatusTable;
---
+++ src/tasks-list.js
@@ -0,0 +1,26 @@
+const tasksList = [
+    {
+        id: 1,
+        name: 'Create tasks list',
+        descr: 'Create file with array objects',
+        status: 1
+    },
+    {
+        id: 2,
+        name: 'Create Board file',
+        descr: 'Create main file with states',
+        status: 1
+    },
+    {
+        id: 3,
+        name: 'Create new task file',
+        descr: 'Create module for add new task',
+        status: 1
+    },
+    {
+        id: 4,
+        name: 'Create status table',
+        descr: 'Create module that shows status tables',
+        status: 1
+    }
+]
