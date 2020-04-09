ee78d1674db7895c96749e9525eed683da180156
aliksayf <52054450+aliksayf@users.noreply.github.com>
2020-04-08 22:54:17 -0700

    New task button

--- src/Board.js
+++ src/Board.js
@@ -1,22 +1,38 @@
 import React, { useState } from 'react';
-import {Col, Container, ListGroup, ListGroupItem, Row} from 'reactstrap';
+import {Col, Container, ListGroup, ListGroupItem, Row, Button } from 'reactstrap';
 import {colors, statusBlock, tasks} from './tasks';
+import NewTask from "./NewTask";
 
 function Board() {
 
     const [taskList, setTaskList] = useState(tasks);
+    const [modal, setModal] = useState(false);
+
+
+
+    const addNewTask = (obj) => {
+        const arr = [...taskList];
+        arr.push(obj);
+        setTaskList([...arr]);
+    };
+
+    const toggle = () => setModal(!modal);
 
     return (
         <Container className="themed-container" fluid="lg">
+            <Button color="primary" onClick={toggle}>New task</Button>
+            <NewTask addNewTask={addNewTask}
+                     toggle={toggle}
+                     modal={modal}/>
             <Row>
                 {statusBlock.map((el, idx) =>
                         <Col md='3' color={colors[idx]}>
                             <ListGroup>
-                                <ListGroupItem color={colors[idx]}>
+                                <ListGroupItem color={colors[idx]} key={idx}>
                                     {el}
                                 </ListGroupItem>
                                 {taskList.map(task => task.status === idx + 1 ?
-                                    <ListGroupItem color="secondary">{task.name}</ListGroupItem> : '')}
+                                    <ListGroupItem color="secondary" key={task.id}>{task.name}</ListGroupItem> : '')}
                             </ListGroup>
                         </Col>
                 )}
--- src/NewTask.js
+++ src/NewTask.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-function NewTask() {
+function NewTask(props) {
+
+
+  const {
+      toggle,
+      modal,
+      className
+    } = props;
+
+
+
   return (
-    <div>
-
-    </div>
+      <div>
+        <Modal isOpen={modal} toggle={toggle} className={className}>
+          <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+          <ModalBody>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
   );
 }
 
