e7328f98e37fdb8ee876a3b5837305c95f340860
aliksayf <52054450+aliksayf@users.noreply.github.com>
2020-04-08 23:24:13 -0700

    New task modal

--- src/NewTask.js
+++ src/NewTask.js
@@ -1,30 +1,63 @@
-import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import React, {useState} from 'react';
+import {
+    Button,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    FormText,
+    Col
+} from 'reactstrap';
 
 function NewTask(props) {
 
+    const [newTaskValues, setNewTaskValues] = useState({});
 
-  const {
-      toggle,
-      modal,
-      className
+    const {
+        toggle,
+        modal
     } = props;
 
+    const inputTaskName = () => {
+
+    };
 
 
-  return (
-      <div>
-        <Modal isOpen={modal} toggle={toggle} className={className}>
-          <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-          <ModalBody>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-  );
+    return (
+        <div>
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Create new task</ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup row>
+                            <Col sm="2">
+                                <Label for="exampleFile">Title</Label>
+                            </Col>
+                            <Col>
+                                <Input type="test" name="title"/>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Col sm="2">
+                                <Label for="exampleText">Description</Label>
+                            </Col>
+                            <Col>
+                                <Input type="textarea" name="text" id="exampleText"/>
+                            </Col>
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={toggle}>Save</Button>{' '}
+                    <Button color="secondary" onClick={toggle}>Cancel</Button>
+                </ModalFooter>
+            </Modal>
+        </div>
+    );
 }
 
 export default NewTask;
