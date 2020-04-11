e8b50ffa69dfc23effe319a2bfe56979b0997ad9
aliksayf <52054450+aliksayf@users.noreply.github.com>
2020-04-10 19:38:41 -0700

    Updates

---
+++ precache-manifest.db5c777efef0a0433acc2fb6971a4523.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9920064a472688d0fb7df9f028d7a814",
+    "url": "/react-canban-board/index.html"
+  },
+  {
+    "revision": "45627e31fb26200ceaa5",
+    "url": "/react-canban-board/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8a65ca0989d9f6b91814",
+    "url": "/react-canban-board/static/css/main.6bbd6223.chunk.css"
+  },
+  {
+    "revision": "45627e31fb26200ceaa5",
+    "url": "/react-canban-board/static/js/2.133ef115.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-canban-board/static/js/2.133ef115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a65ca0989d9f6b91814",
+    "url": "/react-canban-board/static/js/main.a36f16f9.chunk.js"
+  },
+  {
+    "revision": "05ef0c7171087846dd5e",
+    "url": "/react-canban-board/static/js/runtime-main.29cb5c85.js"
+  }
+]);
