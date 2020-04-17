0c52275e6aa487f7c2045bac2f8483377ac3042f
aliksayf <52054450+aliksayf@users.noreply.github.com>
2020-04-17 13:17:51 -0700

    range group

--- src/GroupItem.js
+++ src/GroupItem.js
@@ -1,16 +1,42 @@
-import React, {useState} from 'react';
-import { ListGroupItem } from 'reactstrap';
+import React, {useEffect, useState} from 'react';
+import { ListGroupItem, Col, Row, Button } from 'reactstrap';
+import { FiChevronUp, FiChevronDown } from "react-icons/fi";
+
 
 function GroupItem(props) {
 
     const {
         el,
-        color
+        idx,
+        color,
+        changeOrder,
+        taskGroup
     } = props;
 
+
     return (
-        <ListGroupItem color={color}>
-            {el}
+        <ListGroupItem color={color} >
+            <Row className="p-0">
+                <Col>
+                    {el}
+                </Col>
+                <Col>
+                    <div size="sm" className="m-0">
+                        {(idx !== 0) ?
+                            <FiChevronUp
+                                className="d-block pointer"
+                                onClick={() => changeOrder(idx, -1)}
+                            /> : <FiChevronUp className=" d-block inactive"/>}
+                    </div>
+                    <div size="sm" className="m-0">
+                        {(idx !== taskGroup.status.length - 1) ?
+                            <FiChevronDown
+                                className="d-block pointer"
+                                onClick={() => changeOrder(idx, +1)}
+                            /> : <FiChevronDown className=" d-block inactive"/>}
+                    </div>
+                </Col>
+            </Row>
         </ListGroupItem>
     )
 }
--- src/ManageGroupsModal.js
+++ src/ManageGroupsModal.js
@@ -15,7 +15,20 @@ function ManageGroupsModal(props) {
         setManageModal
     } = props;
 
-
+    const changeOrder = (idx, dif) => {
+        const arr1 = [...taskGroup.status];
+        const arr2 = [...taskGroup.colors];
+        const arr3 = [...taskList];
+        arr1.splice(idx, 1)
+        arr1.splice(idx + dif, 0, taskGroup.status[idx])
+        arr2.splice(idx, 1)
+        arr2.splice(idx + dif, 0, taskGroup.colors[idx])
+        arr3.splice(idx, 1)
+        arr3.splice(idx + dif, 0, taskList[idx])
+        setTaskGroup({status: arr1, colors: arr2})
+        setTaskList(arr3)
+        console.log(arr1, arr2, arr3, idx + dif)
+    };
 
     const toggle = () => {
         setManageModal(!manageModal);
@@ -28,7 +41,13 @@ function ManageGroupsModal(props) {
                 <ModalBody>
                     <ListGroup>
                         {taskGroup.status.map(( el, i )=>
-                            <GroupItem key={i} el={el} color={taskGroup.colors[i]}/>
+                            <GroupItem
+                                key={i}
+                                el={el}
+                                idx={i}
+                                changeOrder={changeOrder}
+                                taskGroup={taskGroup}
+                                color={taskGroup.colors[i]}/>
                         )}
                      </ListGroup>
                 </ModalBody>
